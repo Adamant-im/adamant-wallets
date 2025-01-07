408eb3831f3c53c73b24db0baf4ed731c772dcb9
Vladimir Klevtsov <zonda4242@gmail.com>
2025-01-07 12:03:38 +0100

    extension Bundle

--- AdamantWalletsAssetsBundleFinder.swift
+++ AdamantWalletsAssetsBundleFinder.swift
@@ -6,24 +6,8 @@
 //
 import Foundation
 
-extension Foundation.Bundle {
-    public static let adamantWalletsAssets: Bundle = {
-        let bundleName = "AdamantWalletsAssets"
-
-        let candidates = [
-            Bundle.main.resourceURL,
-            Bundle(for: AdamantWalletsAssetsBundleFinder.self).resourceURL,
-            Bundle.main.bundleURL,
-        ]
-
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-        fatalError("unable to find bundle named \(bundleName)")
-    }()
+public extension Bundle {
+    static var adamantWalletsAssets: Bundle {
+        return Bundle.module
+    }
 }
-
-private class AdamantWalletsAssetsBundleFinder {}
