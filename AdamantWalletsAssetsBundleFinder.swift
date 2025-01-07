6c177f8014d35124464a7290211cf0bd4a512259
Vladimir Klevtsov <zonda4242@gmail.com>
2025-01-07 11:35:05 +0100

    add Foundation.Bundle

---
+++ AdamantWalletsAssetsBundleFinder.swift
@@ -0,0 +1,29 @@
+//
+//  AdamantWalletsAssetsBundleFinder.swift
+//  AdamantWalletsAssets
+//
+//  Created by Владимир Клевцов on 7.1.25..
+//
+import Foundation
+
+extension Foundation.Bundle {
+    static let adamantWalletsAssets: Bundle = {
+        let bundleName = "AdamantWalletsAssets"
+
+        let candidates = [
+            Bundle.main.resourceURL,
+            Bundle(for: AdamantWalletsAssetsBundleFinder.self).resourceURL,
+            Bundle.main.bundleURL,
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        fatalError("unable to find bundle named \(bundleName)")
+    }()
+}
+
+private class AdamantWalletsAssetsBundleFinder {}
