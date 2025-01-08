1471918e08ac695d7634d20b91aa7512d7558b90
Vladimir Klevtsov <zonda4242@gmail.com>
2025-01-08 09:15:17 +0100

    change AssetManager

--- AssetManager.swift
+++ AssetManager.swift
@@ -7,66 +7,38 @@
 import Foundation
 
 public struct AssetManager {
-  public static func loadInfoFiles() -> [Any] {
-    var jsonFiles = [Any]()
-    var blockchainIndex = [String: [String: Any]]()
-    
-    guard let generalResourceURL = Bundle.module.url(forResource: "general", withExtension: nil),
-          let blockchainsResourceURL = Bundle.module.url(forResource: "blockchains", withExtension: nil) else {
-      print("Failed to find 'general' or 'blockchains' folders.")
+  public static func loadFilesFromGeneral() -> [Data] {
+    guard let generalResourceURL = Bundle.module.url(forResource: "general", withExtension: nil) else {
+      print("Failed to find 'general' folder.")
       return []
     }
-    
+    return loadInfoFiles(resourceURL: generalResourceURL)
+  }
+  
+  public static func loadFilesFromBlockchains() -> [Data] {
+    guard let blockchainsResourceURL = Bundle.module.url(forResource: "blockchains", withExtension: nil) else {
+      print("Failed to find 'blockchains' folder.")
+      return []
+    }
+    return loadInfoFiles(resourceURL: blockchainsResourceURL)
+  }
+  
+  static func loadInfoFiles(resourceURL: URL) -> [Data] {
+    var jsonDataFiles = [Data]()
     do {
-      let blockchainsEnumerator = FileManager.default.enumerator(at: blockchainsResourceURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+      let enumerator = FileManager.default.enumerator(at: resourceURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
       
-      while let blockchainFileURL = blockchainsEnumerator?.nextObject() as? URL {
-        if blockchainFileURL.lastPathComponent == "info.json" {
+      while let fileURL = enumerator?.nextObject() as? URL {
+        if fileURL.lastPathComponent == "info.json" {
           do {
-            let blockchainData = try Data(contentsOf: blockchainFileURL)
-            if let blockchainJson = try JSONSerialization.jsonObject(with: blockchainData, options: []) as? [String: Any],
-               let symbol = blockchainJson["symbol"] as? String {
-              blockchainIndex[symbol.lowercased()] = blockchainJson
-            }
+            let data = try Data(contentsOf: fileURL)
+            jsonDataFiles.append(data)
           } catch {
-            print("Failed to parse JSON from blockchains file at \(blockchainFileURL.path): \(error)")
+            print("Failed to load data from file at \(fileURL.path): \(error)")
           }
         }
       }
     }
-    
-    do {
-      let generalEnumerator = FileManager.default.enumerator(at: generalResourceURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-      
-      while let generalFileURL = generalEnumerator?.nextObject() as? URL {
-        if generalFileURL.lastPathComponent == "info.json" {
-          do {
-            let generalData = try Data(contentsOf: generalFileURL)
-            var generalJson = try JSONSerialization.jsonObject(with: generalData, options: []) as? [String: Any]
-            
-            if let symbol = generalJson?["symbol"] as? String,
-               let blockchainJson = blockchainIndex[symbol.lowercased()] {
-              generalJson?.merge(blockchainJson) { (_, blockchainValue) in
-                return blockchainValue
-              }
-            }
-            
-            if let mergedJson = generalJson {
-              jsonFiles.append(mergedJson)
-            }
-          } catch {
-            print("Failed to parse JSON from general file at \(generalFileURL.path): \(error)")
-          }
-        }
-      }
-    }
-    
-    for (symbol, blockchainJson) in blockchainIndex {
-      if !jsonFiles.contains(where: { ($0 as? [String: Any])?["symbol"] as? String == symbol.uppercased() }) {
-        jsonFiles.append(blockchainJson)
-      }
-    }
-    
-    return jsonFiles
+    return jsonDataFiles
   }
 }
