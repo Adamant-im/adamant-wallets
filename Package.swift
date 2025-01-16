8981db5b76d77fdb57d68ec3ef31779c041a71d2
Vladimir Klevtsov <zonda4242@gmail.com>
2025-01-16 23:09:26 +0100

    add folders

--- Package.swift
+++ Package.swift
@@ -2,21 +2,20 @@
 import PackageDescription
 
 let package = Package(
-  name: "AdamantWalletsAssets",
-  platforms: [
-    .iOS(.v15), .macOS(.v10_15)
-  ],
-  products: [
-    .library(
-      name: "AdamantWalletsAssets",
-      targets: ["AdamantWalletsAssets"]
-    ),
-  ],
-  targets: [
-    .target(
-      name: "AdamantWalletsAssets",
-      path: ".",
-      exclude: ["README"]
-    )
-  ]
+    name: "AdamantWalletsAssets",
+    platforms: [
+        .iOS(.v15), .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "AdamantWalletsAssets",
+            targets: ["AdamantWalletsAssets"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "AdamantWalletsAssets",
+            path: "Sources/AdamantWalletsAssets"
+        )
+    ]
 )
